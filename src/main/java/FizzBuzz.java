9b0e9d100d3a9a2d61855f57b35c2c47032f665e
hx <hxfirefox@sina.com>
2016-04-19 19:16:39 +0800

    make say buzz testcase green

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,6 +2,8 @@ public class FizzBuzz {
     public String say(int number) {
         if(3 == number) {
          return "fizz";
+        } else if (5 == number){
+            return "buzz";
         }
         return String.valueOf(number);
     }

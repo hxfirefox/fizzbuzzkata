e3fc64ea89f2e239f92315539919502b14ea640d
hx <hxfirefox@sina.com>
2016-04-19 19:27:42 +0800

    make say fizz when meet 6 test case green

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,13 +1,13 @@
 public class FizzBuzz {
     public String say(int number) {
-        if (3 == number) {
+        if (15 == number) {
+            return "fizzbuzz";
+        }
+        if (number % 3 == 0) {
             return "fizz";
         }
         if (5 == number) {
             return "buzz";
-        }
-        if (15 == number) {
-            return "fizzbuzz";
         }
         return String.valueOf(number);
     }

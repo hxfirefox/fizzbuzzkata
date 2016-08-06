d0782dcb9afd0023f8ae54366a75695ece1adc40
hx <hxfirefox@sina.com>
2016-08-06 22:57:42 +0800

    refactor

--- src/main/java/github/hxfirefox/kata/fizzbuzz/cor/FizzBuzz.java
+++ src/main/java/github/hxfirefox/kata/fizzbuzz/cor/FizzBuzz.java
@@ -1,12 +1,16 @@
 package github.hxfirefox.kata.fizzbuzz.cor;
 
 public class FizzBuzz {
-    public String say(int number) {
+    private static Word word;
+
+    static {
         final Word commonNumberWord = new CommonNumberWord(null);
         final Word buzzWord = new BuzzWord(commonNumberWord);
         final Word fizzWord = new FizzWord(buzzWord);
-        final Word word = new FizzBuzzWord(fizzWord);
+        word = new FizzBuzzWord(fizzWord);
+    }
 
+    public String say(int number) {
         return word.say(number);
     }
 }
